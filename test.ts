9173a2be033aee800c67d4f957bad13f84bb2009
runnerboy22 <runnerboy22@gmail.com>
2024-01-02 15:27:51 -0800

    capturing dimensions and calculating cubic feet

--- test.ts
+++ test.ts
@@ -49,8 +49,6 @@ async function uHaul(url: string): Promise<void> {
     }
   });
 
-  // console.log(locationPricePairs);
-
   // click into each location to determine prices, square ft, and price per sq ft
   // spawn multiple tabs to do this... or just do it sequentially with the back button
 
@@ -59,19 +57,39 @@ async function uHaul(url: string): Promise<void> {
     '#storageResults > li:nth-child(1) > div:nth-child(1) > div.cell.auto > div:nth-child(1) > div.cell.small-8.medium-auto > h3 > a'
   );
 
-  await console.log('clicked into first location');
-
-  // capture data
-  // await page.goto(
-  //   'https://www.uhaul.com/Storage/Walnut-Creek-CA-94598/Results/',
-  //   { waitUntil: 'networkidle2' }
-  // );
-
   await page.waitForNavigation({ waitUntil: 'domcontentloaded' });
 
-  await page.goBack({ waitUntil: 'networkidle2' });
+  const smallDimensions = await page.$$eval(
+    '#small_IndoorStorage_RoomList > li:nth-child(n) > div > div.grid-x.grid-margin-x.align-left.medium-grid-expand-x > div:nth-child(2) > div > div.cell.auto > h4',
+    (elements) =>
+      elements.map((el) => {
+        const dimensions = el.textContent?.trim().split('|')[1].trim() ?? '';
+        const numbers = dimensions
+          .split(' x ')
+          .map((dim: string) => parseInt(dim));
+        return numbers; // returns an array of numbers for each element
+      })
+  );
 
-  await console.log('went back');
+  // smallDimensions is now an array of arrays, each containing three numbers
+  console.log(smallDimensions);
+  // calculate cubic feet
+  const cubicFeet = smallDimensions.map((dim) => {
+    return dim.reduce((acc: number, curr: number) => acc * curr);
+  });
+  console.log(cubicFeet);
+  // for (const [length, width, height] of smallDimensions) {
+  //   console.log(lengthwidth, height);
+  // }
+  // const prices = await page.$$eval(
+
+  // const pricePerSquareFoot = await page.$$eval(
+
+  // capture data
+
+  // await page.goBack({ waitUntil: 'networkidle2' });
+
+  // await console.log('went back');
 
   // setTimeout(async () => {
   //   await browser.close();
