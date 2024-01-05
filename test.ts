ac45e33a9d88a0250c265cec17b86b53b5f8f123
runnerboy22 <runnerboy22@gmail.com>
2024-01-04 23:55:48 -0800

    seond location data collection

--- test.ts
+++ test.ts
@@ -94,11 +94,57 @@ async function uHaul(url: string): Promise<void> {
   });
 
   console.log(smallPricePerCubicFoot);
+
+  // repeat for other locations
+  // might need to handle pagination
+
   // '#small_IndoorStorage_RoomList > li.divider.uhjs-reserve-only > div > div.grid-x.grid-margin-x.align-left.medium-grid-expand-x > div.cell.medium-4.large-3.align-self-top > dl > dd > b'
 
   // const pricePerSquareFoot = await page.$$eval(
 
-  // await page.goBack({ waitUntil: 'networkidle2' });
+  await page.goBack({ waitUntil: 'networkidle2' });
+
+  await page.click(
+    '#storageResults > li:nth-child(2) > div:nth-child(1) > div.cell.auto > div:nth-child(1) > div.cell.small-8.medium-auto > h3 > a'
+  );
+
+  await page.waitForNavigation({ waitUntil: 'domcontentloaded' });
+  // await page.waitForNavigation({ waitUntil: 'networkidle0' });
+
+  const smallDimensions2 = await page.$$eval(
+    '#small_IndoorStorage_RoomList > li:nth-child(n) > div > div.grid-x.grid-margin-x.align-left.medium-grid-expand-x > div:nth-child(2) > div > div.cell.auto > h4',
+    (elements) =>
+      elements.map((el) => {
+        const dimensions = el.textContent?.trim().split('|')[1].trim() ?? '';
+        const numbers = dimensions
+          .split(' x ')
+          .map((dim: string) => parseInt(dim));
+        return numbers; // returns an array of numbers for each element
+      })
+  );
+
+  // smallDimensions is now an array of arrays, each containing three numbers
+  console.log(smallDimensions2);
+  // calculate cubic feet
+  const cubicFeet2 = smallDimensions.map((dim) => {
+    return dim.reduce((acc: number, curr: number) => acc * curr);
+  });
+  console.log(cubicFeet2);
+
+  const smallPrices2 = await page.$$eval(
+    '#small_IndoorStorage_RoomList > li:nth-child(n) > div > div.grid-x.grid-margin-x.align-left.medium-grid-expand-x > div.cell.medium-4.large-3.align-self-top > dl > dd > b',
+    (elements) => elements.map((el) => el.textContent?.trim())
+  );
+
+  console.log(smallPrices2);
+
+  const smallPricePerCubicFoot2 = smallPrices2.map((price, index) => {
+    const numericPrice = parseFloat(price!.replace('$', ''));
+    const pricePerCubicFoot = numericPrice / cubicFeet2[index];
+    return pricePerCubicFoot.toFixed(2);
+  });
+
+  console.log(smallPricePerCubicFoot2);
 
   // await console.log('went back');
 
