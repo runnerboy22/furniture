81db6da25cef3d8346a84e546b0c27db7708d74d
runnerboy22 <runnerboy22@gmail.com>
2024-01-07 01:00:05 -0500

    building location data object

--- test.ts
+++ test.ts
@@ -51,7 +51,19 @@ async function uHaul(url: string): Promise<void> {
       locationPricePairs[trimmedLocation] = trimmedPrice;
     }
   });
-  let allLocations = {};
+
+  type LocationData = {
+    // dimensions: [string];
+    smallDimensions: string[];
+    // smallPrices: [];
+    // smallPricePerCubicFoot: [];
+  };
+  let allLocations: Record<string, LocationData> = {};
+  // let allLocations: { [location: string]: any } = {
+  // smallDimensions: [],
+  // smallPrices: [],
+  // smallPricePerCubicFoot: [],
+  // };
   // start appending to the object to build out the data
   for (let i = 0; i < locations.length; i++) {
     await page.click(
@@ -77,13 +89,10 @@ async function uHaul(url: string): Promise<void> {
         })
     );
 
-    // smallDimensions is now an array of arrays, each containing three numbers
-    console.log(smallDimensions);
-    // calculate cubic feet
     const cubicFeet = smallDimensions.map((dim) => {
       return dim.reduce((acc: number, curr: number) => acc * curr);
     });
-    console.log(cubicFeet);
+    // console.log(cubicFeet);
 
     const smallPrices = await page.$$eval(
       `#small_IndoorStorage_RoomList > li:nth-child(${
@@ -92,7 +101,7 @@ async function uHaul(url: string): Promise<void> {
       (elements) => elements.map((el) => el.textContent?.trim())
     );
 
-    console.log(smallPrices);
+    // console.log(smallPrices);
 
     const smallPricePerCubicFoot = smallPrices.map((price, index) => {
       const numericPrice = parseFloat(price!.replace('$', ''));
@@ -100,103 +109,110 @@ async function uHaul(url: string): Promise<void> {
       return pricePerCubicFoot.toFixed(2);
     });
 
-    console.log(smallPricePerCubicFoot);
+    // console.log(smallPricePerCubicFoot);
 
     await page.goBack({ waitUntil: 'networkidle2' });
+    // if (locations[i]) {
+    allLocations[locations[i]] = {
+      smallDimensions,
+      // Add other data as needed
+    };
+    console.log(allLocations);
+    // }
   }
 
-  await page.click(
-    '#storageResults > li:nth-child(1) > div:nth-child(1) > div.cell.auto > div:nth-child(1) > div.cell.small-8.medium-auto > h3 > a'
-  );
+  // await page.click(
+  //   '#storageResults > li:nth-child(1) > div:nth-child(1) > div.cell.auto > div:nth-child(1) > div.cell.small-8.medium-auto > h3 > a'
+  // );
 
-  await page.waitForNavigation({ waitUntil: 'domcontentloaded' });
-  // await page.waitForNavigation({ waitUntil: 'networkidle0' });
+  // await page.waitForNavigation({ waitUntil: 'domcontentloaded' });
+  // // await page.waitForNavigation({ waitUntil: 'networkidle0' });
 
-  const smallDimensions = await page.$$eval(
-    '#small_IndoorStorage_RoomList > li:nth-child(n) > div > div.grid-x.grid-margin-x.align-left.medium-grid-expand-x > div:nth-child(2) > div > div.cell.auto > h4',
-    (elements) =>
-      elements.map((el) => {
-        const dimensions = el.textContent?.trim().split('|')[1].trim() ?? '';
-        const numbers = dimensions
-          .split(' x ')
-          .map((dim: string) => parseInt(dim));
-        return numbers; // returns an array of numbers for each element
-      })
-  );
+  // const smallDimensions = await page.$$eval(
+  //   '#small_IndoorStorage_RoomList > li:nth-child(n) > div > div.grid-x.grid-margin-x.align-left.medium-grid-expand-x > div:nth-child(2) > div > div.cell.auto > h4',
+  //   (elements) =>
+  //     elements.map((el) => {
+  //       const dimensions = el.textContent?.trim().split('|')[1].trim() ?? '';
+  //       const numbers = dimensions
+  //         .split(' x ')
+  //         .map((dim: string) => parseInt(dim));
+  //       return numbers; // returns an array of numbers for each element
+  //     })
+  // );
 
-  // smallDimensions is now an array of arrays, each containing three numbers
-  console.log(smallDimensions);
-  // calculate cubic feet
-  const cubicFeet = smallDimensions.map((dim) => {
-    return dim.reduce((acc: number, curr: number) => acc * curr);
-  });
-  console.log(cubicFeet);
+  // // smallDimensions is now an array of arrays, each containing three numbers
+  // console.log(smallDimensions);
+  // // calculate cubic feet
+  // const cubicFeet = smallDimensions.map((dim) => {
+  //   return dim.reduce((acc: number, curr: number) => acc * curr);
+  // });
+  // console.log(cubicFeet);
 
-  const smallPrices = await page.$$eval(
-    '#small_IndoorStorage_RoomList > li:nth-child(n) > div > div.grid-x.grid-margin-x.align-left.medium-grid-expand-x > div.cell.medium-4.large-3.align-self-top > dl > dd > b',
-    (elements) => elements.map((el) => el.textContent?.trim())
-  );
+  // const smallPrices = await page.$$eval(
+  //   '#small_IndoorStorage_RoomList > li:nth-child(n) > div > div.grid-x.grid-margin-x.align-left.medium-grid-expand-x > div.cell.medium-4.large-3.align-self-top > dl > dd > b',
+  //   (elements) => elements.map((el) => el.textContent?.trim())
+  // );
 
-  console.log(smallPrices);
+  // console.log(smallPrices);
 
-  const smallPricePerCubicFoot = smallPrices.map((price, index) => {
-    const numericPrice = parseFloat(price!.replace('$', ''));
-    const pricePerCubicFoot = numericPrice / cubicFeet[index];
-    return pricePerCubicFoot.toFixed(2);
-  });
+  // const smallPricePerCubicFoot = smallPrices.map((price, index) => {
+  //   const numericPrice = parseFloat(price!.replace('$', ''));
+  //   const pricePerCubicFoot = numericPrice / cubicFeet[index];
+  //   return pricePerCubicFoot.toFixed(2);
+  // });
 
-  console.log(smallPricePerCubicFoot);
+  // console.log(smallPricePerCubicFoot);
 
-  // repeat for other locations
-  // might need to handle pagination
+  // // repeat for other locations
+  // // might need to handle pagination
 
-  // '#small_IndoorStorage_RoomList > li.divider.uhjs-reserve-only > div > div.grid-x.grid-margin-x.align-left.medium-grid-expand-x > div.cell.medium-4.large-3.align-self-top > dl > dd > b'
+  // // '#small_IndoorStorage_RoomList > li.divider.uhjs-reserve-only > div > div.grid-x.grid-margin-x.align-left.medium-grid-expand-x > div.cell.medium-4.large-3.align-self-top > dl > dd > b'
 
-  // const pricePerSquareFoot = await page.$$eval(
+  // // const pricePerSquareFoot = await page.$$eval(
 
-  await page.goBack({ waitUntil: 'networkidle2' });
+  // await page.goBack({ waitUntil: 'networkidle2' });
 
-  await page.click(
-    '#storageResults > li:nth-child(2) > div:nth-child(1) > div.cell.auto > div:nth-child(1) > div.cell.small-8.medium-auto > h3 > a'
-  );
+  // await page.click(
+  //   '#storageResults > li:nth-child(2) > div:nth-child(1) > div.cell.auto > div:nth-child(1) > div.cell.small-8.medium-auto > h3 > a'
+  // );
 
-  await page.waitForNavigation({ waitUntil: 'domcontentloaded' });
-  // await page.waitForNavigation({ waitUntil: 'networkidle0' });
+  // await page.waitForNavigation({ waitUntil: 'domcontentloaded' });
+  // // await page.waitForNavigation({ waitUntil: 'networkidle0' });
 
-  const smallDimensions2 = await page.$$eval(
-    '#small_IndoorStorage_RoomList > li:nth-child(n) > div > div.grid-x.grid-margin-x.align-left.medium-grid-expand-x > div:nth-child(2) > div > div.cell.auto > h4',
-    (elements) =>
-      elements.map((el) => {
-        const dimensions = el.textContent?.trim().split('|')[1].trim() ?? '';
-        const numbers = dimensions
-          .split(' x ')
-          .map((dim: string) => parseInt(dim));
-        return numbers; // returns an array of numbers for each element
-      })
-  );
+  // const smallDimensions2 = await page.$$eval(
+  //   '#small_IndoorStorage_RoomList > li:nth-child(n) > div > div.grid-x.grid-margin-x.align-left.medium-grid-expand-x > div:nth-child(2) > div > div.cell.auto > h4',
+  //   (elements) =>
+  //     elements.map((el) => {
+  //       const dimensions = el.textContent?.trim().split('|')[1].trim() ?? '';
+  //       const numbers = dimensions
+  //         .split(' x ')
+  //         .map((dim: string) => parseInt(dim));
+  //       return numbers; // returns an array of numbers for each element
+  //     })
+  // );
 
-  // smallDimensions is now an array of arrays, each containing three numbers
-  console.log(smallDimensions2);
-  // calculate cubic feet
-  const cubicFeet2 = smallDimensions.map((dim) => {
-    return dim.reduce((acc: number, curr: number) => acc * curr);
-  });
-  console.log(cubicFeet2);
+  // // smallDimensions is now an array of arrays, each containing three numbers
+  // console.log(smallDimensions2);
+  // // calculate cubic feet
+  // const cubicFeet2 = smallDimensions.map((dim) => {
+  //   return dim.reduce((acc: number, curr: number) => acc * curr);
+  // });
+  // console.log(cubicFeet2);
 
-  const smallPrices2 = await page.$$eval(
-    '#small_IndoorStorage_RoomList > li:nth-child(n) > div > div.grid-x.grid-margin-x.align-left.medium-grid-expand-x > div.cell.medium-4.large-3.align-self-top > dl > dd > b',
-    (elements) => elements.map((el) => el.textContent?.trim())
-  );
+  // const smallPrices2 = await page.$$eval(
+  //   '#small_IndoorStorage_RoomList > li:nth-child(n) > div > div.grid-x.grid-margin-x.align-left.medium-grid-expand-x > div.cell.medium-4.large-3.align-self-top > dl > dd > b',
+  //   (elements) => elements.map((el) => el.textContent?.trim())
+  // );
 
-  console.log(smallPrices2);
+  // console.log(smallPrices2);
 
-  const smallPricePerCubicFoot2 = smallPrices2.map((price, index) => {
-    const numericPrice = parseFloat(price!.replace('$', ''));
-    const pricePerCubicFoot = numericPrice / cubicFeet2[index];
-    return pricePerCubicFoot.toFixed(2);
-  });
+  // const smallPricePerCubicFoot2 = smallPrices2.map((price, index) => {
+  //   const numericPrice = parseFloat(price!.replace('$', ''));
+  //   const pricePerCubicFoot = numericPrice / cubicFeet2[index];
+  //   return pricePerCubicFoot.toFixed(2);
+  // });
 
-  console.log(smallPricePerCubicFoot2);
+  // console.log(smallPricePerCubicFoot2);
 
   // await console.log('went back');
 
